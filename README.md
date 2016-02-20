0a165a0f5281c9e4e856ab6bec471f42e29a66f1
Rory McDaniel <rorycmcdaniel@gmail.com>
2016-02-19 23:35:19 -0500

    initial commit to repository

---
+++ README.md
@@ -0,0 +1,12 @@
+## Synopsis
+
+A web based remote for controlling your Roku. Because you can never find the remote, and sometimes your phone battery is dead.
+
+## Setup
+
+On first load, an alert window will ask you for the IP address of your Roku. It then sets a cookie and will not ask for the IP
+address again for 365 days. You can clear this cookie at any time by clicking the clear cookie link in the bottom right hand corner.
+
+## Contributors
+
+If you'd like to contribute, fork the repo, then create a pull request.
---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+/* Cookie functions taken from w3schools example: http://www.w3schools.com/js/js_cookies.asp
+ */
+var ip_address;
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var ip = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCookie() {
+    var cookie = getCookie("ip");
+    var ip = cookie.split("=");
+    if (cookie) {
+       ip_address = ip[1];
+    } else {
+        ip = prompt("Enter the IP Address of your Roku:", "");
+        if (ip != "" && ip != null) {
+            setCookie("ip", ip, 365);
+            ip_address = ip;
+        }
+    }
+}
+$(document).ready(function(){
+
+    while(!ip_address){
+        checkCookie();
+    }
+
+    $("button").click(function(){
+        var value = $(this).attr('id');
+        var url = 'http://' + ip_address + ':8060/keypress/' + value;
+        $.post(url);
+        return false;
+    });
+
+    $("a.clear_cookies").click(function(){
+        document.cookie = "ip=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+        alert("The ip address cookie has been cleared. You will be prompted again for your IP address.");
+        location.reload();
+    });
+});
